5c2f5327775006c69915cd922c5debb6c5ecc654
eadr-fil <fildv79@gmail.com>
2017-05-24 12:48:45 +0300

    Create base structure

---
+++ LetterGeneratorSolution/Domain/Concrete/OracleReestrRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class OracleReestrRepository : IReestrRepository
+    {
+        public IEnumerable<Reestr> Reestrs
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ LetterGeneratorSolution/Domain/Entities/Reestr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Reestr
+    {
+        public int Reestr_5_ID { get; set; }
+
+        //public string ReestrName { get; set; }
+
+        //public string ReestrCreditor { get; set; }
+
+        //public string ReestrDescription { get; set; }
+    }
+}
